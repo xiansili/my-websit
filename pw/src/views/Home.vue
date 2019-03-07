36ddc02227c12af48e7620497f294882fdd1b188
xiansili <865915497@qq.com>
2019-03-07 21:47:08 +0800

    web

--- pw/src/views/Home.vue
+++ pw/src/views/Home.vue
@@ -33,7 +33,7 @@ export default {
   width: 100%;
   height: 100%;
   background-size: cover;
-  background-clip: content-box;
+ background-position:center;
   background-image: url("../assets/imgs/home/home3.jpg");
   perspective: 2000px;
   
