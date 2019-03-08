ef46bc5a0cc152087b80e0f26e1398f36f255714
xiansili <865915497@qq.com>
2019-03-08 12:36:03 +0800

    web

--- pw/src/views/Home.vue
+++ pw/src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="wrapper">
-    <!-- <div class="wrapper-cover"></div> -->
-    <!-- <div class="wrapper-me">
-      <div class="wrapper-me-front cube">
-        <h2 class="cube-name">I am xian si li</h2>
-        <strong class="cube-welecome">欢迎来到我的网站</strong>
-        <p class="cube-name">我叫先思力</p>
-        <p class="cube-job">我是一名前端开发工程师</p>
+    <div class="wrapper-cover"></div>
+    <div class="wrapper-me">
+      <div class="wrapper-me left">
+        <h2 class="wrapper-me-name">I am xian si li</h2>
+        <strong class="wrapper-me-welecome">欢迎来到我的网站</strong>
+        <p class="wrapper-me-name">我叫先思力</p>
+        <p class="wrapper-me-job">我是一名前端开发工程师</p>
       </div>
-      <div class="wrapper-me-left cube"></div>
-      <div class="wrapper-me-right cube"></div>
-      <div class="wrapper-me-back cube"></div>
-    </div> -->
+    </div>
     <div class="wrapper-next">
       <router-link to="/about">
         <img src="@/assets/imgs/next.png" alt="向下">
@@ -36,69 +33,34 @@ export default {
   background-position: left center;
   background-image: url("../assets/imgs/home/home3.jpg");
   perspective: 2000px;
-  
+
   &-me {
     position: relative;
-    width: 700px;
-    height: 280px;
+    width: 900px;
+    height: 400px;
     top: 50%;
     left: 50%;
-    margin-top: -140px;
-    margin-left: -350px;
-    transform-origin: 50% 50%;
-    transform-style: preserve-3d;
+    margin-top: -150px;
+    margin-left: -450px;
     color: #000;
-
-    &:hover {
-      animation: mycube 10s linear;
-      @keyframes mycube {
-        0% {
-          transform: rotateY(0deg);
-        }
-        100% {
-          transform: rotateY(-360deg);
-        }
-      }
+    background: #d3d7d4 0.5;
+    border: 2px solid #000;
+    &-name,
+    &-job {
+      text-align: center;
     }
-
-    & .cube {
-      width: 700px;
-      height: 280px;
-      position: absolute;
-      background-position: center;
-      background-size: cover;
-      // background: #d3d7d4;
-      // border: 2px solid #000;
-      background: #fff;
-      &-name,
-      &-job {
-        text-align: center;
-      }
-      &-welecome {
-        float: left;
-        width: 100%;
-        text-align: center;
-        margin-top: 20px;
-      }
+    &-welecome {
+      float: left;
+      width: 100%;
+      text-align: center;
+      margin-top: 20px;
     }
-    &-front {
-      transform: translateZ(350px);
-      background-image: url("../assets/imgs/home/home2.jpg");
-    }
-    &-left {
-      transform: rotateY(90deg) translateZ(350px);
-      background-image: url("../assets/imgs/home/home1.jpg");
-    }
-    &-right {
-      transform: rotateY(-90deg) translateZ(350px);
-      background-image: url("../assets/imgs/home/home3.jpg");
-    }
-    &-back {
-      transform: rotateY(180deg) translateZ(350px);
-      background-image: url("../assets/imgs/home/home5.jpg");
+    & .left{
+     border: 2px solid orange;
+     height: 100%;
+     width: 50%;
     }
   }
-
   //下一页面箭头
   &-next {
     position: absolute;
